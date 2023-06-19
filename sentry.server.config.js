fabd7602189a7eb464c63d3301972c2b2e9fcc42
Semih Yonet <semih@shoppingminds.com>
2023-06-20 00:33:09 +0200

    added sentry

---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d26ece4e58b74cea8beb0f15aa223ecf@o4505210114146304.ingest.sentry.io/4505387364646912",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
