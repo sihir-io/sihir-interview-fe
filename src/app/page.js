94ad9a81eb9db0c854178b4bbdbfd2f316b67134
Yigithan Saglam <ysaglam1@binghamton.edu>
2023-10-29 19:44:31 -0400

    Disable forwarding for testing

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,6 @@
 import {useEffect} from "react";
 
 export default function Home() {
-//    To directly redirect users to a different webpage use the following:
-    useEffect(() => {
-        window.location.href = 'https://sihir.io'
-    }, [])
-
-
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             Welcome to Sihirterview
