9033822402144abca26c68dec7606e3c8f44c213
Yigithan Saglam <ysaglam1@binghamton.edu>
2023-10-29 19:54:29 -0400

    Test container deployment

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import {useEffect} from "react";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            Welcome to Sihirterview
+            Welcome to Sihir Interview!
         </main>
     )
 }
